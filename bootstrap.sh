e57907c6c00cc983655bb769af6030f3208c0668
Corwin Harris <git@corwin.betabeetle.com>
2018-12-14 14:02:02 -0500

    Fixed another typo

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
-ansible-playbook -K -i ansible/inventory ansible/playbook.yml
+ansible-playbook -K -i ansible/inventory.yml ansible/playbook.yml
 
