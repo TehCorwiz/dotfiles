03937a4e4106250580034a5cf4ac106158bb4888
Corwin Harris <git@corwin.betabeetle.com>
2018-12-14 13:00:39 -0500

    Added first draft of Ansible config Added speedtest command Added bootstrap script to isntall and run ansible

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Install Ansible from official ppa using apt
+sudo apt update
+sudo apt install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible -y
+
+ansible-playbook -K -i ansible/inventory ansible/playbook.yml
+
