2a0990fa4cb2309e540a3f24a7961fec4dfa02b5
Corwin Harris <git@corwin.betabeetle.com>
2019-11-17 16:02:25 -0500

    Fxied misc bugs and typos

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
-ansible-galaxy install fubarhouse.rust
+ansible-galaxy install naftulikay.rust-dev
 ansible-playbook -K -i inventory.yml playbook.yml
 
