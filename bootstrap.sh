455eaca1f690679f56aebb3e4afb8d9175f84f3b
Corwin Harris <git@corwin.betabeetle.com>
2022-09-14 16:20:44 -0400

    Added zotero

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,6 @@ sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
 
+ansible-galaxy collection install community.general
 ansible-galaxy install naftulikay.rust-dev
 ansible-playbook -K -i inventory.yml playbook.yml
